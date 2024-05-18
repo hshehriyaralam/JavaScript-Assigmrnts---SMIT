be12f2b7bbf2347a7be1a43da61f2034539ecdf8
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-18 14:32:57 -0600

    Chapter 2

---
+++ Chapter 1/app.js
@@ -0,0 +1,5 @@
+alert("Please Enter a Valid Password")
+alert("Welcome to JS  Land \n Happy Coding!")
+alert("Happy coding! \n Prevent this page from creating additional value")
+alert("Hello .... I can run JS through my web browser console")
+
---
+++ chapter 2/app.js
@@ -0,0 +1,30 @@
+var userName = "Sherry";
+var myName = "Hafiz Shehriyar Alam";
+
+var JSVariable = "This is Chapter 2 ";
+var message = "Hello World";
+alert(JSVariable);
+alert(message);
+
+var stdName = "Jhone Doe";
+var stdAge = "15 years Old";
+var stdCourse = "Certified Mobile Appication Development";
+
+alert(stdName);
+alert(stdAge);
+alert(stdCourse);
+
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+var userEmail = "hshehriyaralam@gmail.com";
+alert(`My email is ${userEmail}`);
+
+var calledBook = "' A smarter way to learn JavaScript '";
+alert(`I am trying to learn from the Book ${calledBook}`);
+
+
+var HTMLText = "Yah! I can write HTML content through JavaScript"
+document.write(HTMLText);
+
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
