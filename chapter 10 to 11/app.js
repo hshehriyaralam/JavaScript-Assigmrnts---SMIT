d6dd224bc7e69989a8dadc57c7c0b669f1846f9b
hshehriyaralam <hshehriyaralam@gmail.com>
2024-06-02 13:44:03 -0600

    chapter 10 half

---
+++ chapter 10 to 11/app.js
@@ -0,0 +1,106 @@
+
+//  function citySubmit(){
+// var input = document.getElementById("Now");
+// if(input.value === "karachi"){
+//     alert("welcome the city Of light")
+// }else{
+//     alert("Chla Ja")
+// }
+//  };
+
+
+//  function ageSubmitt(){
+//     var userAge = document.getElementById("age");
+//     if(userAge.value === "male"){
+//         alert("Good Morning Sir")
+//     }else if( userAge.value === "female"){
+//         alert("Good Morning Ma’am");
+//     }else{
+//         alert("Again Soja")
+//     }
+//  };
+
+// function trafficSignal(){
+//     var colors = document.getElementById("Signal");
+//     if(colors.value === "Red"){
+//         alert("Must Stop")
+//     }else if(colors.value === "Yellow"){
+//         alert("Ready to move")
+//     }else if(colors.value === "Green" ){
+//         alert("Move now")
+//     }else{
+//         alert("Jaa Ghar Jaa")
+//     }
+// }
+
+// function Refillfuel(){
+//     var fuels = document.getElementById("fuel");
+//     if(fuels.value <= 0.25){
+//         alert("Please refill the fuel in  your car ")
+//     }else{
+//         alert ("Chala Jaa")
+//     }
+// }
+// Script
+// var a = 4
+// if (++a === 5){
+//     alert("given condition for variable a is true")
+// }   
+
+
+// var b = 82;
+// if(b++ === 83){
+//     alert("given condition for variable b is true")
+//     console.log("Alow")
+// }else{
+//     console.log("Not ALLow");
+// }
+
+
+
+// var c = 12;
+// if (c++ === 13){
+//     alert("condition 1 is true") //false
+    
+// }
+// if (c === 13){
+//     alert("condition 2 is true") //true
+// }
+// if(++c < 14){
+//     alert("condition 3 is true") //false
+// }
+// if(c === 14){
+//     alert("condition 4 is true") // true
+// }
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if(totalCost === laborCost + materialCost){
+//     alert("The cost Equals")
+// };
+
+// if(true){
+//     alert("true")
+// };
+//  if(false){
+//     alert("false")
+//  };
+
+
+
+
+
+
+if ("car" < "cat"){
+    alert("car is smaller than cat")
+};
+
+
+
+
+
+
+
+
